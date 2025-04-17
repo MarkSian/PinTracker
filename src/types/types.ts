c0f2689e47d6bd1abeba7249300d7d8946206160
MarkSian <merksianipar@gmail.com>
2025-04-17 14:26:52 -0600

    server listening

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,26 +1,22 @@
 // Below are the types used in this project.
-// Frame will track the rolls in a frame, whether it is a strike or spare, and the frame number.
-// Game will track the date, total score, and the resulting frames from that game.
-// Bowler will track the name, average score, and the games played by the bowler.
 
 
-export type Bowler = {
-    id: string; // we did not use a number as a string allows us to be more flexibile in how we generate the id
-    name: string;
-    averageScore: number;
-    games: Game[]; //array of games played by the bowler
-};
-
-export type Game = { // a Game is a single game played by a bowler
+export type Team = {
+    id: string; // unique id for the team
+    name: string; // team name
+    bowlerIds: string[]; //array of bowlers on the team
+  };
+  
+  export type Bowler = {
     id: string;
-    date: Date;
+    name: string;
+    averageScore: number; // average score of the bowler from all games
+    games: Game[]; // array of games played by the bowler
+    teamId: string | null; // id of the team the bowler belongs to, null if not in a team
+  };
+  
+  export type Game = {
+    id: string;
+    date: string;
     totalScore: number;
-    frames: Frame[]; // array of the frames within the game
-};
-
-export type Frame = { // a Frame is at most two throws (or three in the 10th frame) in a game
-    frameNumber: number; // the number of the frame (1-10)
-    rolls: number []; // array of the rolls in the frame
-    isStrike: boolean;
-    isSpare: boolean;
-};
+  };
